51a6df8ae1db1c584da79104d1493d10d8d9f79a
Mateusz Lis <mateusz.a.lis@gmail.com>
2023-04-12 20:51:42 +0200

    Task 1 Finished

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyColor = document.body;
 
+let timerId = null;
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    bodyColor.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  console.log(`Interval with id ${timerId} has stopped!`);
+  startBtn.disabled = false;
+});
