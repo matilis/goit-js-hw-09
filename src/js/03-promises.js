fc9d50ae8a1a7d0fe1cc13b66c3c7cfca71758f3
Mateusz Lis <mateusz.a.lis@gmail.com>
2023-04-19 22:22:44 +0200

    Task 3 Finished small update 2

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,14 +6,16 @@ const amountEl = document.querySelector('input[name="amount"]');
 const btnEl = document.querySelector('button[type="submit"]');
 
 function createPromise(position, delay) {
-  setTimeout(() => {
-    const shouldResolve = Math.random() > 0.3;
-    if (shouldResolve) {
-      resolve({ position, delay });
-    } else {
-      reject({ position, delay });
-    }
-  }, delay);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
 
 btnEl.addEventListener('click', event => {
